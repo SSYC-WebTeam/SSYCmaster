a00a85197361dea8bc4441d9e52777f1ca1366c8
slin0013 <slin0013@seattlecentral.edu>
2015-10-29 16:25:59 -0700

    add footer details
    add footer details

--- footer.php
+++ footer.php
@@ -1,17 +1,68 @@
-//footer.php
-
+<!-- footer starts here -->		
 <?php 
 
 ?>
-<footer>
-	<ul>
-    <li><a href="index.html">Copyright</a></li>
-    <li><a href="">All Rights Reserved</a></li>
-    <li><a href="">Disclaimer</a></li>
-	<li><a href="">Web Design by SSYC WebTeam</a></li>
-    <li><a href="">CONTACT</a></li>
-	</ul>
-</footer>
+<div id="footer">
+<hr />
+	<div class="footerTitle">
+		<h4>Home</h4>
+	</div>
+	
+		<ul class="footerText">	
+		    <li><a href="index1.php">Home</a></li>
+			<li><a href="index2.php">Home</a></li>
+		</ul>
+
+	<div class="footerTitle">
+		<h4>MEMBERSHIP</h4>
+	</div>
+	
+		<ul class="footerText">	
+			<li><a href="membership1.php">How it works</a></li>
+			<li><a href="membership2.php">Meetings</a></li>
+			<li><a href="membership3.php">Pricing</a></li>
+		</ul>
+
+	<div class="footerTitle">
+		<h4>BOATING</h4>
+	</div>
+	
+		<ul class="footerText">	
+		    <li><a href="boating1.php">Home</a></li>
+			<li><a href="boating2.php">Home</a></li>
+			<li><a href="boating3.php">Home</a></li>
+		</ul>
+			
+	<div class="footerTitle">
+		<h4>EVENTS</h4>
+	</div>
+	
+		<ul class="footerText">	
+		    <li><a href="events1.php">Home</a></li>
+			<li><a href="events2.php">Home</a></li>
+			<li><a href="events3.php">Home</a></li>				
+		</ul>
+
+	<div class="footerTitle">
+		<h4>NEWS</h4>
+	</div>
+	
+		<ul class="footerText">	
+		    <li><a href="news1.php">Home</a></li>
+			<li><a href="news2.php">Home</a></li>
+			<li><a href="news3.php">Home</a></li>				
+		</ul>	
+
+	<div class="footerTitle">
+		<h4>CONTACT US</h4>
+	</div>
+	
+		<ul class="footerText">	
+		    <li><a href="contact1.php">Home</a></li>
+			<li><a href="contact2.php">Home</a></li>
+			<li><a href="contact3.php">Home</a></li>				
+		</ul>	
+</div>
 
 <?php wp_footer(); ?>
 

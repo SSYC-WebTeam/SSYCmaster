af794ef8d7de82908435a5f50085769722cf5287
slin0013 <slin0013@seattlecentral.edu>
2015-10-30 01:35:33 -0700

    add link list
    add link list

---
+++ links.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<div id="wrapper">
+<div id="content">
+
+<?php 
+
+?>
+
+<h2>HOW IT WORKS</h2>
+  <ul>
+    <?php get_links_list();?>
+  </ul>
+
+</div>	
+<?php get_sidebar(); ?>
+</div>

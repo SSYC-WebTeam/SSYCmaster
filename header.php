3dd5a9efb9d812848459bccf237631a3d2881838
slin0013 <slin0013@seattlecentral.edu>
2015-11-03 19:37:40 -0800

    Revert "add navlist"
    This reverts commit 2e6ce4e3db70a18c98dc25ce42bbb9fd3167aff0.

--- header.php
+++ header.php
@@ -10,37 +10,23 @@
 <meta name="viewport" content="width=device-width"/>
 <meta http-equiv="content-type" content="text/html; charset=US-ASCII" />
 
-<title><?php wp_title() ?></title>
-
-<!-- Style Sheets-->
 <link href='http://fonts.googleapis.com/css?family=Varela+Round|Carter+One|Kalam|Lemon|Monoton|Wire+One|Tulpen+One' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Audiowide|Michroma|Julius+Sans+One' rel='stylesheet' type='text/css'>
 
+<title><?php wp_title() ?></title>
+
 <link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen">
-
-<![endif]-->
-
 <?php wp_head(); ?>
-  
+    
 </head>
 
-<body <?php body_class($class); ?>>
-
-<div id="header">
-	<div id="logo">
-       <p>SEATTLE SINGLES YACHT CLUB</p>
-    </div>
-</div>
-
-	<div id="menu">
-		<nav>
-			<ul>
-				<li><a href="membership.php">MEMBERSHIP</a></li>
-				<li><a href="boating.php">BOATING</a></li>
-				<li><a href="events.php">EVENTS</a></li>
-				<li><a href="news.php">NEWS</a></li>
-				<li><a href="contact.php">CONTACT US</a></li>
-			</ul>	
-		</nav>
-	</div><!-- end navigation bar -->
-
+<body>
+    <header>
+        <div id="logo">
+            <p>SSYC</p>
+        </div>
+    </header>
+    
+ <nav>
+	<?php wp_nav_menu( array('theme_location' => 'main')) ?>
+</nav>

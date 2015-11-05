e7032c2fe0042ea7cc842f7d1855ceac904cb2b9
slin0013 <slin0013@seattlecentral.edu>
2015-11-05 03:27:15 -0800

    add post
    add post

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
 <div id="wrapper">
-    
-<aside>
-<?php get_sidebar(); ?>
-
-</aside>
-    
-<main>
-    <div id="content">
-
-	<?php if (have_posts()) : ?>
+ 	 	<?php if (have_posts()) : ?>
 		
-    <?php while (have_posts()) : the_post(); ?>
-				
-    <h2><a href="<?php the_permalink() ?>" <?php the_title(); ?>></a></h2>
-	
+        <?php while (have_posts()) : the_post(); ?>
+
+	<article class="post">			
+        <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title();?></a></h2>
+
+		<div class="entry-content"><!--//post-->
+			<?php the_content();?>
+		</div><!--//.entry-content-->
+	</article>
+
 	<?php endwhile; ?>
 	
 	<?php else : ?>
-	
+		<h2 class="center">Not Found</h2>
+		<p class="center">Sorry, but you are looking for something that isn't here.</p>
+			<?php get_search_form(); ?>	
 	<?php endif; ?>
-	
-	</div>
+</div>
 
-</main>
+<aside>
+<?php get_sidebar(); ?>
+</aside>
     
 <?php get_footer(); ?>

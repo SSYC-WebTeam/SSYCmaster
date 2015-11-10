180b025a71b6c10ff67a96f875484af28872f7d6
maureencrist <maureencrist@comcast.net>
2015-11-10 14:27:39 -0800

    Registering Nav

--- functions.php
+++ functions.php
@@ -1,14 +1,26 @@
 <?php
 
-function register_my_menus() {
-  register_nav_menus(
-      array(
-        'main-menu' => __('Main Menu'),
-		'mobile-menu' => __('Mobile Menu'),
-        'footer-menu' => __('Footer Menu')
-        )
-      );
+//function register_my_menus() {
+  //register_nav_menus(
+    //  array(
+      //  'main-menu' => __('Main Menu'),
+		//'mobile-menu' => __('Mobile Menu'),
+        //'footer-menu' => __('Footer Menu')
+        //)
+      //);
+//}
+
+//Register custom menus
+if ( function_exists( 'register_nav_menus' ) ) {
+	register_nav_menus(
+		array(
+		  'main-menu' => 'Main Menu',
+		  'mobile-menu' => 'Mobile Menu',
+		  'footer-menu' => 'Footer Menu'
+		)
+	);
 }
+
 add_action( 'init', 'register_my_menus' );
 
 ?>

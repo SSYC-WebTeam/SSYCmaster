824f0664a78f51e5d5e172939fcd83fa5bf446d0
maureencrist <maureencrist@comcast.net>
2015-11-10 14:01:42 -0800

    Updated functions.php
    trying to fix navigation.

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function register_my_menus() {
   register_nav_menus(
       array(
-        'main' => __('Main Menu'),
+        'main-menu' => __('Main Menu'),
         'footer-menu' => __('Footer Menu')
         )
       );

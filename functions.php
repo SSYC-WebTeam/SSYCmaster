c85728232f3ba5266eee098c3e5977f5bbcbb94e
slin0013 <slin0013@seattlecentral.edu>
2015-11-18 00:16:37 -0800

    changed front sidebar/ add image placehold
    changed front sidebar/ add image placehold

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 //function register_my_menu() {
-	if ( function_exists( 'register_nav_menus' ) ) {
+if ( function_exists( 'register_nav_menus' ) ) {
 	register_nav_menus(array(
 		  'main-menu' => 'Main Menu',
 		  'toggle-menu' => 'Mobile Toggle Menu',
@@ -10,33 +10,6 @@
 add_action( 'init', 'register_my_menu' );
 
 function widgets_init() {	
-	register_sidebar (array(
-		'name' => __( 'Left Sidebar', 'widgets_init' ),
-		'id' => 'left-front',
-		'class' => 'one-third-left',
-		'before_widget' => '<li id="%1$s" class="widget %2$s">',
-		'after_widget' => '</li>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>' ));
-
-	register_sidebar (array(
-		'name' => __( 'Main Sidebar', 'widgets_init' ),
-		'id' => 'main-front',
-		'class' => 'one-third-middle',
-		'before_widget' => '<li id="%1$s" class="widget %2$s">',
-		'after_widget' => '</li>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>' ));
-
-	register_sidebar (array(
-		'name' => __( 'Right Sidebar', 'widgets_init' ),
-		'id' => 'right-front',
-		'class' => 'one-third-right last',
-		'before_widget' => '<li id="%1$s" class="widget %2$s">',
-		'after_widget' => '</li>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>' ));
-
 	register_sidebar (array(
 		'name' => __( 'First Top Sidebar', 'widgets_init' ),
 		'id' => 'left-widget-top',
@@ -60,6 +33,5 @@ function widgets_init() {
 		'after_widget' => '</li>',
 		'before_title' => '<h2 class="widgettitle">',
 		'after_title' => '</h2>' ));
-
 }
 add_action( 'init', 'widgets_init' );
--- sidebar.php
+++ sidebar.php
@@ -1,24 +1,24 @@
-<aside id="front"> 
+<aside id="aside-front"> 
 <!-- #left sidebar -->
         <div class="one-third-left">
-                <?php if ( is_active_sidebar( 'left-front' ) ) : ?>
-                <?php dynamic_sidebar( 'left-front' ); ?>
-                <?php endif; ?>                
-        </div> <!-- end .one-third left-->
+            <img src="http://dummyimage.com/70x70" />
+            <h4>UPCOMING EVENTS</h4>
+               <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam. Wisi aperiam ad duo, cu vidit graecis est, nam assum tritani instructior ne. Nulla tritani perfecto vix et, ut nam altera laoreet, posidonium concludaturque est an. Duo harum nominati no, cu dicant nostrum argumentum has</p>
+        </div> <!-- end .one-third -->
 
 <!-- #main sidebar -->           
         <div id="main-front">
-            <div class="one-third-middle">                
-                <?php if ( is_active_sidebar( 'main-front' ) ) : ?>
-                <?php dynamic_sidebar( 'main-front' ); ?>
-                <?php endif; ?>                       
+            <div class="one-third-middle">
+            <img src="http://dummyimage.com/70x70" /> 
+            <h4>RECENT NEWS</h4>
+               <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam. Wisi aperiam ad duo, cu vidit graecis est, nam assum tritani instructior ne. Nulla tritani perfecto vix et, ut nam altera laoreet, posidonium concludaturque est an. Duo harum nominati no, cu dicant nostrum argumentum has</p>
             </div> <!-- end .one-third middle -->
  
 <!-- #right sidebar -->                  
-            <div class="one-third-right last">                
-                <?php if ( is_active_sidebar( 'right-front' ) ) : ?>
-                <?php dynamic_sidebar( 'right-front' ); ?>
-                <?php endif; ?>                
-            </div> <!-- end .one-third -->                             
+        <div class="one-third-right last">
+            <img src="http://dummyimage.com/70x70" /> 
+            <h4>SEO PARAGRAPH</h4>
+               <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam. Wisi aperiam ad duo, cu vidit graecis est, nam assum tritani instructior ne. Nulla tritani perfecto vix et, ut nam altera laoreet, posidonium concludaturque est an. Duo harum nominati no, cu dicant nostrum argumentum has</p>
+            </div> <!-- end .one-third -->                    
          </div> <!-- end #main-front -->  
 </aside>

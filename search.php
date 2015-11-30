48f7b8baf5fa7f3ceeef62503aaffb6bde80cc2c
slin0013 <slin0013@seattlecentral.edu>
2015-11-30 00:27:04 -0800

    add search page
    add search page

---
+++ search.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+ <div class="wrapper">
+        <?php if (have_posts()) : ?>
+             <h2 class="pagesearch">Search Results</h2> 
+
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="post">
+                <small><?php the_time('F jS, Y, l') ?></small>  
+                <h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>                          
+                <p class="postcategory">Posted in <?php the_category(', ') ?></p>
+            </div>
+        <?php endwhile; ?>
+
+        <?php else : ?>
+            <h2 class="center">Not Found</h2>
+            <p class="center">Sorry, but you are looking for something that isn't here.</p>
+                <?php get_search_form(); ?> 
+        <?php endif; ?>
+
+    <aside>    
+        <?php get_sidebar(); ?>
+    </aside>
+</div> <!-- end #wrapper --> 
+
+<?php get_footer(); ?>

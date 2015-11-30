7bbb225f339931770e87f4b6a7cc0703b4215338
joshbyrd <jbyrd004@seattlecnetral.edu>
2015-11-29 17:52:07 -0800

    footer submenu fix

--- 404.php
+++ 404.php
@@ -1,7 +1,8 @@
 <?php get_header() ?>
 
 	<div class="page404">
-        <h2>Error 404 - Page Not Found.</h2>	
+        <h2>Error 404 - Page Not Found.</h2>
+        <img src="images/ssyc-logo.png" alt="Seattle Single Yacht Club 404 error page" >
 	</div>
 		
 <?php get_sidebar(); ?>

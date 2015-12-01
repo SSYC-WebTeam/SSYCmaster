2f49e66c9d3bc3a9485f8d3e046f7487813e0c43
slin0013 <slin0013@seattlecentral.edu>
2015-11-30 23:53:01 -0800

    add sidebar page
    add sidebar page

--- functions.php
+++ functions.php
@@ -11,24 +11,24 @@ add_action( 'init', 'register_my_menu' );
 
 function widgets_init() {	
 	register_sidebar (array(
-		'name' => __( 'First Top Sidebar', 'widgets_init' ),
-		'id' => 'left-widget-top',
+		'name' => __( 'Top Sidebar', 'widgets_init' ),
+		'id' => 'widget-top',
 		'before_widget' => '<li id="%1$s" class="widget %2$s">',
 		'after_widget' => '</li>',
 		'before_title' => '<h2 class="widgettitle">',
 		'after_title' => '</h2>' ));
 
 	register_sidebar (array(
-		'name' => __( 'Second middle Sidebar', 'widgets_init' ),
-		'id' => 'left-widget-middle',
+		'name' => __( 'Middle Sidebar', 'widgets_init' ),
+		'id' => 'widget-middle',
 		'before_widget' => '<li id="%1$s" class="widget %2$s">',
 		'after_widget' => '</li>',
 		'before_title' => '<h2 class="widgettitle">',
 		'after_title' => '</h2>' ));
 
 	register_sidebar (array(
-		'name' => __( 'Third bottom Sidebar', 'widgets_init' ),
-		'id' => 'left-widget-bottom',	
+		'name' => __( 'Bottom Sidebar', 'widgets_init' ),
+		'id' => 'widget-bottom',	
 		'before_widget' => '<li id="%1$s" class="widget %2$s">',
 		'after_widget' => '</li>',
 		'before_title' => '<h2 class="widgettitle">',
--- sidebar.php
+++ sidebar.php
@@ -1,37 +1,23 @@
-<aside id="aside-front"> 
-<!-- #left sidebar -->
-        <div class="one-third-left">
-            <img src="http://localhost:8080/wp-content/uploads/2015/11/calendar-icon.png" alt="calendar icon"/>
-            <div class="textbox">
-                <h3>UPCOMING EVENTS</h3>
-                <h4>Loret Lipsum</h4>
-                <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. </p>
-                <h4>Quisque ultrices</h4>
-                <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. </p>
-             </div> <!-- end .textbox-->  
-        </div> <!-- end .one-third -->
+<aside id="primary">
+<!-- #top sidebar -->
+    <div class="top sidebar">
+        <?php if ( is_active_sidebar( 'widget-top' ) ) : ?>        
+        <?php dynamic_sidebar( 'widget-top' ); ?>
+        <?php endif; ?>
+    </div><!-- end .top sidebar-->
 
-<!-- #main sidebar -->           
-        <div id="main-front">
-            <div class="one-third-middle">
-            <img src="http://localhost:8080/wp-content/uploads/2015/11/news-icon.png" alt="news-icon" />
-            <div class="textbox">
-                <h3>RECENT NEWS</h3>
-                <h4>Loret Lipsum</h4>
-                <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. </p>
-                <h4>Quisque ultrices</h4>
-                <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. </p>
-             </div> <!-- end .textbox-->  
-            </div> <!-- end .one-third middle -->
- 
-<!-- #right sidebar -->                  
-        <div class="one-third-right last">
-            <img src="http://localhost:8080/wp-content/uploads/2015/11/anchor-icon.png" alt="anchor-icon"/>
-            <div class="textbox">
-                <h3>SEO PARAGRAPH</h3>
-                <h4>Loret Lipsum</h4>
-                <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet.</p>
-             </div> <!-- end .textbox-->  
-            </div> <!-- end .one-third -->                    
-         </div> <!-- end #main-front -->  
+<!-- #middle sidebar -->
+    <div class="middle sidebar">
+        <?php if ( is_active_sidebar( 'widget-middle' ) ) : ?>        
+        <?php dynamic_sidebar( 'widget-middle' ); ?>
+        <?php endif; ?>
+    </div><!-- end .middle sidebar--> 
+
+<!-- #bottom sidebar -->
+    <div class="bottom sidebar">
+        <?php if ( is_active_sidebar( 'widget-bottom' ) ) : ?>        
+        <?php dynamic_sidebar( 'widget-bottom' ); ?>
+        <?php endif; ?>
+    </div><!-- end .bottom sidebar-->   
 </aside>
+

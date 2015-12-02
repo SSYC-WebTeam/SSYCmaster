924fb59ba2c24249e07575fc30e74414a8287acb
joshbyrd <jbyrd004@seattlecnetral.edu>
2015-12-01 19:15:24 -0800

    add 404 image

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 
 	<div class="page404">
         <h2>Error 404 - Page Not Found.</h2>
-        <img src="images/ssyc-logo.png" alt="Seattle Single Yacht Club 404 error page" >
+        <img src="<?php bloginfo('url'); ?>/images/ssyc-logo.png" alt="Seattle Single Yacht Club 404 error page" >
 	</div>
 		
 <?php get_sidebar(); ?>

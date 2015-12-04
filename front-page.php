a68683205f04fc6a552f0fac35c76e874fa069f1
slin0013 <slin0013@seattlecentral.edu>
2015-12-03 22:49:59 -0800

    change front sidebar content to sidebar.php

--- front-page.php
+++ front-page.php
@@ -1,12 +1,10 @@
 <?php get_header(); ?>
 
-<div id="middle">
-    
+<div id="middle">    
 
        <div class="slideshow"><!--Slideshow-->
        		<img src="<?php bloginfo('url'); ?>/wp-content/themes/SSYCmaster/images/front-1900x510-1.jpg" alt="slides" />
         </div>
-
     
         <?php if (have_posts()) : ?>
         
@@ -16,49 +14,14 @@
         
         <?php else : ?>
             <h2 class="center">Not Found</h2>
-            <p class="center">Sorry, but you are looking for something that isn't here.</p>
-                <?php get_search_form(); ?> 
+            <p class="center">Sorry, but you are looking for something that isn't here.</p>              
         <?php endif; ?>
         
 	<div class="wrapper">
     
-  		<aside id="aside-front"> 
-			<!-- #left sidebar -->
-        	<div class="one-third-left">
-                <img src="<?php bloginfo('url'); ?>/wp-content/themes/SSYCmaster/images/calendar-icon.png" alt="calendar icon"/>
-                <div class="textbox">
-                    <h3>UPCOMING EVENTS</h3>
-                    <h4>Loret Lipsum</h4>
-                    <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. </p>
-                    <h4>Quisque ultrices</h4>
-                    <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. </p>
-                 </div> <!-- end .textbox-->  
-            </div> <!-- end .one-third -->
-
-			<!-- #main sidebar -->           
-            <div id="main-front">
-                <div class="one-third-middle">
-                <img src="<?php bloginfo('url'); ?>/wp-content/themes/SSYCmaster/images/news-icon.png" alt="news-icon" />
-                <div class="textbox">
-                    <h3>RECENT NEWS</h3>
-                    <h4>Loret Lipsum</h4>
-                    <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. </p>
-                    <h4>Quisque ultrices</h4>
-                    <p>Lorem ipsum dolor sit amet, iisque imperdiet definiebas id nam imperdiet. </p>
-                 </div> <!-- end .textbox-->  
-                </div> <!-- end .one-third middle -->
-     
-    		<!-- #right sidebar -->                  
-            <div class="one-third-right last">
-                <img src="<?php bloginfo('url'); ?>/wp-content/themes/SSYCmaster/images/anchor-icon.png" alt="anchor-icon"/>
-                <div class="textbox">
-                    <h3>SSYC</h3>
-                    <h4>We saved you a seat!</h4>
-                    <p>Seattle Singles Yacht Club is an active organization of single adults who love boating and socializing, both on and off the water. The club welcomes experienced skippers, sailors and crew, as well as singles with minimal boating experience.</p>
-                 </div> <!-- end .textbox-->  
-                </div> <!-- end .one-third -->                    
-             </div> <!-- end #main-front -->  
-		</aside>
+        <aside id="aside-front"> 
+            <?php get_sidebar(); ?>
+        </aside>
  
 	</div> <!--end .wrapper-->
 

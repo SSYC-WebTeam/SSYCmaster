c490c362403024b13522e17724824664a4266097
maureencrist <maureencrist@comcast.net>
2015-12-03 19:57:34 -0800

    Update Footer and Styling for Footer

--- footer.php
+++ footer.php
@@ -7,18 +7,23 @@
         <nav id="nav-footer">
         
         	<div class="wrapper">
+            
+            	<div class="contact">
+                    <ul>
+                        <li>CONTACT</li>
+                    </ul>
+               	</div> 
+                
+                <ul class="sub-contact">
+                    <li><img class="img-contact" src="<?php bloginfo('url'); ?>/wp-content/themes/SSYCmaster/images/phone-icon.png" alt="phone"/>  (206)555-1234</li>
+                    <li><img class="img-contact" src="<?php bloginfo('url'); ?>/wp-content/themes/SSYCmaster/images/mail-icon.png" alt="email"/>   contact@ssyc.org</li>
+                </ul>
+        
 				<?php wp_nav_menu( array( 
                     'theme_location' => 'footer-menu' ,
                     'menu' => 'Footer Menu' ,
                     'container'  => 'ul' )); ?> 
                     
-          		<div class="contact">
-                	<ul>
-                    	<li><img src="<?php bloginfo('url'); ?>/wp-content/themes/SSYCmaster/images/images/phone-icon.png" alt="phone" />(206)555-1234</li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>     
 
 			</div> <!-- end .wrapper -->
 

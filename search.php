6950565806a6dda528f3908d3c2f1f9cd392de4d
slin0013 <slin0013@seattlecentral.edu>
2015-12-06 04:26:26 -0800

    delete search form

--- search.php
+++ search.php
@@ -14,8 +14,7 @@
 
         <?php else : ?>
             <h2 class="center">Not Found</h2>
-            <p class="center">Sorry, but you are looking for something that isn't here.</p>
-                <?php get_search_form(); ?> 
+            <p class="center">Sorry, but you are looking for something that isn't here.</p>               
         <?php endif; ?>
 
         <aside id="aside-front"> 
--- single.php
+++ single.php
@@ -16,7 +16,6 @@
         <?php else : ?>
             <h2 class="center">Not Found</h2>
             <p class="center">Sorry, but you are looking for something that isn't here.</p>
-                <?php get_search_form(); ?> 
         <?php endif; ?>
 
         <aside id="aside-front"> 

2b273f91f02e4b1ec7af66708dfb9eda688ab293
Luis Gamboa <luisgamboasierra@gmail.com>
2015-12-06 15:19:27 -0800

    add social icons and main menu focus
    Get rid of phone number and add social icons below. Fixe the hover in
    main menus

--- footer.php
+++ footer.php
@@ -15,8 +15,13 @@
                	</div> 
                 
                 <ul class="sub-contact">
-                    <li><img class="img-contact" src="<?php get_stylesheet_directory_uri(); ?>/wp-content/themes/SSYCmaster/images/phone-icon.png" alt="phone"/>  (206)555-1234</li>
-                    <li><img class="img-contact" src="<?php get_stylesheet_directory_uri(); ?>/wp-content/themes/SSYCmaster/images/mail-icon.png" alt="email"/>   contact@ssyc.org</li>
+
+                    <li><img class="img-contact" src="<?php bloginfo('url'); ?>/wp-content/themes/SSYCmaster/images/mail-icon.png" alt="email"/>   contact@ssyc.org</li>
+                    
+                    <li><a class="soc-facebook" href="#"></a></li>
+                    <li><a class="soc-instagram soc-icon-last" href="#"></a></li>
+
+
                 </ul>
         
 				<?php wp_nav_menu( array( 

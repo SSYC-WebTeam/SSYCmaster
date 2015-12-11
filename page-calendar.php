4278d5cc47d9d5515ba447c0bcdd9d3e87ebb234
joshbyrd <jbyrd004@seattlecnetral.edu>
2015-12-10 19:42:11 -0800

    page-calendar.php

---
+++ page-calendar.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+<body id="page-template">
+
+     <div id="middle">
+     
+         <div class="wrapper">
+                        
+             <div id="breadcrumbs">
+                <?php the_breadcrumb(); ?>
+             </div> <!-- #breadcrumbs -->
+                
+             <aside id="primary">
+             
+            </aside> <!-- end #primary -->                                
+            
+            <div id="content">    
+               	<?php if (have_posts()) : ?>
+                
+                <?php while (have_posts()) : the_post(); ?>
+                        
+                <h2><?php the_title();?></h2>
+        
+                <!--post-->
+                <?php the_content();?>
+                
+                <?php endwhile; ?>
+                
+                <?php else : ?>
+                        <h2 class="center">Not Found</h2>
+                        <p class="center">Sorry, but you are looking for something that isn't here.</p>
+                
+                <?php endif; ?>
+        
+                </div> <!-- end #content -->  
+     	
+        </div> <!-- end .wrapper -->                         
+    </div> <!-- end #middle -->
+</body><!--end page-template-->
+
+<?php get_footer(); ?>

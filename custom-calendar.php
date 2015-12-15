c17b05c549293a71dc38c4bd30d48fe5ba150e69
slin0013 <slin0013@seattlecentral.edu>
2015-12-14 16:05:06 -0800

    add breadcrumb for calendar page

--- custom-calendar.php
+++ custom-calendar.php
@@ -12,10 +12,50 @@ Template Name: Calendar Template
          <div class="wrapper">
                         
              <div id="breadcrumbs">
-                <?php the_breadcrumb(); ?>
+              <?php 
+                    global $post;   
+                    if (!is_home()) {
+                        echo '<a href="';
+                        echo get_option('home');
+                        echo '">';
+                        bloginfo('name');
+                        echo "</a> / ";
+
+                    if (is_category() || is_single()) {
+                            the_category('title_li=');
+                            if (is_single()) {
+                                echo " / ";
+                                the_title();
+                            }
+
+                            } elseif(tribe_is_month()) {
+                                echo '<a href="'.tribe_get_events_link().'">CALENDAR</a>';
+                                echo " / ";
+                                echo "<strong>";
+                                echo '<a href="'.tribe_is_month().'">MONTHLY EVENTS</a>';
+                                echo "</strong>";
+                            } else if(tribe_is_list_view() && ! is_tax()) {
+                                echo '<a href="'.tribe_get_events_link().'">CALENDAR</a>';
+                                echo " / ";
+                                echo "<strong>";
+                                echo '<a href="'.tribe_is_list_view().'">EVENT LISTS</a>';
+                                echo "</strong>";
+                            } else if(tribe_is_day() && ! is_tax()) {
+                                echo '<a href="'.tribe_get_events_link().'">CALENDAR</a>';
+                                echo " / ";
+                                echo "<strong>";
+                                echo '<a href="'.tribe_is_day().'">SINGLE DAY</a>';
+                                echo "</strong>";
+                            } else if(tribe_is_day()) {
+                                echo 'SINGLE DAY';
+                            } else {
+                                the_title();
+                            }
+                    }
+             ?>
              </div> <!-- #breadcrumbs -->
                 
-             <aside id="primary">
+            <aside id="primary">
              
                              
             </aside> <!-- end #primary -->                                

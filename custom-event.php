ea75ccad18a30f26c46a386e22f2296a6c5a014a
joshbyrd <jbyrd004@seattlecnetral.edu>
2015-12-15 12:40:38 -0800

    adding list view in sidebar
    update to events page

--- custom-event.php
+++ custom-event.php
@@ -17,14 +17,7 @@ Template Name: Event Page Template
                  
              </div> <!-- #breadcrumbs -->
                  
-              <?php 
-                    global $post;   
-                    if (!is_home()) {
-                        echo '<a href="';
-                        echo get_option('home');
-                        echo '">';
-                        bloginfo('name');
-                        echo "</a> / ";
+                    <?php 
 
                     if (is_category() || is_single()) {
                             the_category('title_li=');
@@ -59,9 +52,9 @@ Template Name: Event Page Template
                     }
              ?>
                 
-            <aside id="primary">
+<aside id="primary">
                 
-                <?php
+<?php
 /**
  * Events Pro Mini Calendar Widget
  * This is the template for the output of the mini calendar widget.
@@ -108,12 +101,84 @@ $args = tribe_events_get_mini_calendar_args();
 		tribe_get_template_part( 'pro/widgets/mini-calendar/list', null, array( 'venue' => true ) );
 	}
 	?>
+    
+    
+    
+<?php
+/**
+ * Events Pro List Widget Template
+ * This is the template for the output of the events list widget.
+ * All the items are turned on and off through the widget admin.
+ * There is currently no default styling, which is highly needed.
+ *
+ * Override this template in your own theme by creating a file at [your-theme]/tribe-events/pro/widgets/list-widget.php
+ *
+ * When the template is loaded, the following vars are set:
+ *
+ * @var string $start
+ * @var string $end
+ * @var string $venue
+ * @var string $address
+ * @var string $city
+ * @var string $state
+ * @var string $province
+ * @var string $zip
+ * @var string $country
+ * @var string $phone
+ * @var string $cost
+ * @var array  $instance
+ *
+ * @package TribeEventsCalendarPro
+ *
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+// Retrieves the posts used in the List Widget loop.
+$posts = tribe_get_list_widget_events();
+
+// The URL for this widget's "View More" link.
+$link_to_all = tribe_events_get_list_widget_view_all_link( $instance );
+
+// Check if any posts were found.
+if ( isset( $posts ) && $posts ) :
+
+	foreach ( $posts as $post ) :
+		setup_postdata( $post );
+		do_action( 'tribe_events_widget_list_inside_before_loop' ); ?>
+
+		<!-- Event  -->
+		<div class="<?php tribe_events_event_classes() ?>">
+			<?php tribe_get_template_part( 'pro/widgets/modules/single-event', null, $instance ) ?>
+		</div>
+
+		<?php do_action( 'tribe_events_widget_list_inside_after_loop' ) ?>
+
+	<?php endforeach ?>
+
+	<p class="tribe-events-widget-link">
+		<a href="<?php esc_attr_e( esc_url( $link_to_all ) ) ?>" rel="bookmark">
+			<?php esc_html_e( 'View More&hellip;', 'tribe-events-calendar-pro' ) ?>
+		</a>
+	</p>
+
+<?php
+// No Events were found.
+else:
+?>
+	<p><?php printf( __( 'There are no upcoming %s at this time.', 'tribe-events-calendar-pro' ), strtolower( tribe_get_event_label_plural() ) ); ?></p>
+<?php
+endif;
+
+// Cleanup. Do not remove this.
+wp_reset_postdata();
+
+?>
 </div>
-
-             
-                             
-            </aside> <!-- end #primary -->                                
+                   
+    </aside> <!-- end #primary -->                                
             
             <div id="content">    
                	<?php if (have_posts()) : ?>

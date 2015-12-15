c357567d4001e7100b3e5b6217997e83b473e9d3
joshbyrd <jbyrd004@seattlecnetral.edu>
2015-12-15 12:29:17 -0800

    adding even-page template

--- custom-event.php
+++ custom-event.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Calendar Template
+Template Name: Event Page Template
 */
 ?>
 <?php get_header(); ?>
@@ -14,17 +14,10 @@ Template Name: Calendar Template
              <div id="breadcrumbs">
                  
                  <?php the_breadcrumb(); ?>
-      
+                 
              </div> <!-- #breadcrumbs -->
-                
-            <aside id="primary">
-                
-                    
-            </aside> <!-- end #primary -->                                
-            
-            <div id="content">    
-              
-                <?php 
+                 
+              <?php 
                     global $post;   
                     if (!is_home()) {
                         echo '<a href="';
@@ -65,8 +58,82 @@ Template Name: Calendar Template
                             }
                     }
              ?>
+                
+            <aside id="primary">
+                
+                <?php
+/**
+ * Events Pro Mini Calendar Widget
+ * This is the template for the output of the mini calendar widget.
+ *
+ * Override this template in your own theme by creating a file at [your-theme]/tribe-events/pro/widgets/mini-calendar-widget.php
+ *
+ * @package TribeEventsCalendarPro
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+$args = tribe_events_get_mini_calendar_args();
+
+?>
+
+<!-- Removing this wrapper class will break the claendar javascript, please avoid and extend as needed -->
+
+<div class="tribe-mini-calendar-wrapper">
+
+	<!-- Grid -->
+	<?php
+	$month_widget_args = array(
+		'tax_query' => $args['tax_query'],
+		'eventDate' => $args['eventDate'],
+		'suppress_nothing_found_notice' => true,
+		'tribe_render_context' => 'widget',
+	);
+
+	/**
+	 * Filter Mini Calendar Widget tribe_show_month args
+	 *
+	 * @param array $month_widget_args Arguments for the Mini Calendar Widget's call to tribe_show_month
+	 */
+	$month_widget_args = apply_filters( 'tribe_events_pro_min_calendar_widget_query_args', $month_widget_args );
+
+	tribe_show_month( $month_widget_args, 'pro/widgets/mini-calendar/grid' ); ?>
+
+	<!-- List -->
+	<?php
+	if ( 0 < $args['count'] ) {
+		tribe_get_template_part( 'pro/widgets/mini-calendar/list', null, array( 'venue' => true ) );
+	}
+	?>
+
+</div>
+
+             
+                             
+            </aside> <!-- end #primary -->                                
+            
+            <div id="content">    
+               	<?php if (have_posts()) : ?>
+                
+                <?php while (have_posts()) : the_post(); ?>
+                        
+                <h2><?php the_title();?></h2>
         
-            </div> <!-- end #content -->  
+                <!--post-->
+                <?php the_content();?>
+                
+                <?php endwhile; ?>
+                
+                <?php else : ?>
+                        <h2 class="center">Not Found</h2>
+                        <p class="center">Sorry, but you are looking for something that isn't here.</p>
+                
+                <?php endif; ?>
+        
+                </div> <!-- end #content -->  
      	
         </div> <!-- end .wrapper -->                         
     </div> <!-- end #middle -->
